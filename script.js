b0295d4cfcb3fded8e22ad4cffbabc0fcce784e7
sharunspi <sharunkk1997@gmail.com>
2021-06-17 19:25:20 +0530

    added checking

--- script.js
+++ script.js
@@ -2,16 +2,29 @@ let star ="<div class='star'> X </div>"
 let zero = "<div class='zero'> O </div>"
 const cards =document.querySelectorAll('.card')
 cards.forEach(card=>{
-    card.addEventListener('click',function(){
-        console.log(this.innerHTML)
-        if(this.innerHTML ==zero||this.innerHTML ==star ){
+    card.addEventListener('click',function(ev){
+        ev.preventDefault();
+        if(this.children.length >0){
             alert("Can't edit this field")
-        }else{
+        }
+        else{
             this.innerHTML=zero
         }
+
+        checkTally()
     })
     card.addEventListener('contextmenu', function(ev) {
         ev.preventDefault();
-        this.innerHTML = star
+        if(this.children.length >0){
+            alert("Can't edit this field")
+        }
+        else{
+            this.innerHTML = star
+        }
+        
+        checkTally()
     }, false)
 })
+function checkTally(){
+
+}
